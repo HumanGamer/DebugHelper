10b2f2ab828771586934ee3082e7800faeed99b8
Matthieu Parizeau <mattparizeau@hotmail.com>
2018-01-08 00:36:47 -0500

    Added Code For DebugViewer

---
+++ DebugHelper/DebugUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace DebugHelper
+{
+    public static class DebugUtil
+    {
+        public static void ShowDebug(object obj)
+        {
+            Form dlg = DebugViewer.BuildDebugDlg(obj);
+            dlg.ShowDialog();
+        }
+
+        public static void ShowDebug(IWin32Window owner, object obj)
+        {
+            Form dlg = DebugViewer.BuildDebugDlg(obj);
+            dlg.ShowDialog(owner);
+        }
+    }
+}
